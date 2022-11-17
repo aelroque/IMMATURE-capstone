6d5173abbf30d1b41f41c4304712c94bf8622a59
beezeebug <aelroque.semings@gmail.com>
2022-11-17 14:49:17 +0800

    task/Nov/172022 - Footer with date and time

--- frontend/src/component/Footer.js
+++ frontend/src/component/Footer.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+
 
 const Footer = () => {
+  const [clockState, setClockState] = useState();
+  const [dateState, setDateState] = useState();
+
+
+  useEffect(() => {
+    setInterval(() => {
+      const date = new Date();
+      setClockState(date.toLocaleTimeString());
+      setDateState(date.toLocaleDateString());
+    }, 1000);
+  }, []);
+  
   return (
     <footer>
+      <h3 className="text-white text-center">{clockState} {dateState }</h3>
       <div className="text-white text-center py-3">
         immature &copy; All rights reserved. 2022
       </div>
