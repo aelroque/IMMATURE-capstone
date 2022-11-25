ba848ffa1144c7e3528c019d6458b78646d58cc3
beezeebug <aelroque.semings@gmail.com>
2022-11-25 19:16:16 +0800

    page title

--- frontend/src/component/search/Search.js
+++ frontend/src/component/search/Search.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Container } from 'react-bootstrap';
+import { Helmet } from 'react-helmet-async';
 
 function ProductSearch() {
     const [products, setProducts] = useState([])
@@ -14,19 +16,30 @@ function ProductSearch() {
     }, []);
 
     return (
+      <Container className="small-container">
+        <Helmet>
+          <title>Price List</title>
+        </Helmet>
+
+        <h1 className="my-3">Price List</h1>
         <div className="App">
-            <input
-                type="text"
-                placeholder="Search"
-                className="search"
-                onChange={(e) => setQuery(e.target.value)}
-            />
-            <ul className="list">
-                {products.filter(product=>product.pname.toLowerCase().includes(query)).map((product) => (
-                    <li key={product.pid} className="lisItem">{product.pname} : {product.srp}</li>
-                ))}
-            </ul>
+          <input
+            type="text"
+            placeholder="Search"
+            className="search"
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <ul className="list">
+            {products
+              .filter((product) => product.pname.toLowerCase().includes(query))
+              .map((product) => (
+                <li key={product.pid} className="lisItem">
+                  {product.pname} : {product.srp}
+                </li>
+              ))}
+          </ul>
         </div>
-    )
+      </Container>
+    );
 }
 export default ProductSearch;
